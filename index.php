d8dd8cf3934faeddb8d8831e018af0d27a301565
Imelda Medina <imedina-castaneda@mail.greenriver.edu>
2020-01-15 14:44:32 -0500

    addded a view

--- index.php
+++ index.php
@@ -7,3 +7,15 @@
 //Turn on error reporting
 ini_set('display_errors',1);
 error_reporting(E_ALL);
+//Require autoload file
+require("vendor/autoload.php");
+//Instantiate F3
+$f3 = Base:: instance();
+
+//define a default route
+$f3->route('GET /', function(){
+    $view = new Template();
+    echo $view->render('views/all-about-chickens.html');
+});
+//Run F3
+$f3->run();
