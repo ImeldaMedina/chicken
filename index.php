1078d2b007455d0031ee40afb7a6693756b4cec7
Imelda Medina <imedina-castaneda@mail.greenriver.edu>
2020-02-10 14:15:49 -0500

    updating index.php views and composer

--- index.php
+++ index.php
@@ -10,12 +10,27 @@ error_reporting(E_ALL);
 //Require autoload file
 require("vendor/autoload.php");
 //Instantiate F3
+
+
 $f3 = Base:: instance();
+
+//Instantiate controller object
+$controller= new ChickenController($f3);
 
 //define a default route
 $f3->route('GET /', function(){
-    $view = new Template();
-    echo $view->render('views/all-about-chickens.html');
+    global $controller;
+    $controller->home();
+
+//    $GLOBALS['controller']->home();
 });
+
+//define a default route
+$f3->route('GET /eggs', function() {
+    $GLOBALS['controller']->eggs();
+//    global $controller;
+//    $controller->eggs();
+});
+
 //Run F3
 $f3->run();
